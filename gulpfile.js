fd9c0aebe310691994354e728bd26819fdcdf1df
Коберник Евгений Валерьевич <ev_kobernik@BBGROUP.LOCAL>
2017-08-14 18:19:31 +0300

    Html v1.0

--- gulpfile.js
+++ gulpfile.js
@@ -30,10 +30,6 @@ gulp.task('styles', function () {
 
 gulp.task('scripts', function() {
 	return gulp.src([
-		'./app/libs/modernizr/modernizr.js',
-		'./app/libs/jquery/jquery-1.11.2.min.js',
-		'./app/libs/waypoints/waypoints.min.js',
-		'./app/libs/animate/animate-css.js',
 		])
 		.pipe(concat('libs.js'))
 		// .pipe(uglify()) //Minify libs.js
